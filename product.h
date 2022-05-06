e4a011cf0d94f16c9670aae0c5933e889e0111f8
s21900156 <100466772+s21900156@users.noreply.github.com>
2022-05-06 23:29:48 +0900

    Update product.h

--- product.h
+++ product.h
@@ -9,6 +9,6 @@ void readProduct(Product p); //메뉴 조회
 int updateProduct(Product *p); //메뉴 수정
 int deletProduct(Product *p); //메뉴 삭제
 void saveData(Product *p, int count); //FILE 저장 
-int loadData(Prodcut *p); //FILE 로드
+int loadData(Produt *p); //FILE 로드
 void searchProductName(Product *p, int count); //제품명으로 검색
 
