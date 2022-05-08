62a0859d07eb8c7e5a0aa57b0614b3659504c750
s21900156 <100466772+s21900156@users.noreply.github.com>
2022-05-09 00:20:23 +0900

    Update product.h
    비밀번호 불러오는 함수추가

--- product.h
+++ product.h
@@ -10,8 +10,9 @@ void readProduct(Product p); //메뉴 조회
 int updateProduct(Product *p); //메뉴 수정
 int deletProduct(Product *p); //메뉴 삭제
 void listProduct(Product *p, int count); //메뉴 리스트 조회
-int selectProductNo(Product *p, int count); //원하는 메뉴 선택
 void saveData(Product *p, int count); //FILE 저장 
-int loadData(Produt *p); //FILE 로드
+int loadData(Product *p); //FILE 로드
 void searchProductName(Product *p, int count); //제품명으로 검색
 int changePassword(int oldPassword);//비밀번호 변경
+int selectProductNo(Product *p, int count); //원하는 메뉴 선택
+int loadPassword();//비밀번호 불러오기
