f347dab706cac225672c64137242e87382bcabbf
DESKTOP-KV0QQ70\주헌 <andamiro@handong.ac.kr>
2022-05-08 17:56:01 +0900

    add select function

--- product.h
+++ product.h
@@ -9,6 +9,7 @@ int createProduct(Product *p); //메뉴를 추가
 void readProduct(Product p); //메뉴 조회
 int updateProduct(Product *p); //메뉴 수정
 int deletProduct(Product *p); //메뉴 삭제
+int selectProductNo(Product *p, int count); //원하는 메뉴 선택
 void saveData(Product *p, int count); //FILE 저장 
 int loadData(Produt *p); //FILE 로드
 void searchProductName(Product *p, int count); //제품명으로 검색
