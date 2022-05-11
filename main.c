1a8841af24f4bd41c9c764e48c34268c797ad542
s21900156 <100466772+s21900156@users.noreply.github.com>
2022-05-11 21:57:53 +0900

    Update main.c
    포장 매장이용 확인함수 추가

--- main.c
+++ main.c
@@ -7,10 +7,12 @@ int main (void) {
 	Product plist[50];
 	int index = 0;
 	int count =0, menu;
+	int count2 =0, index2=0;//이용한 손님 수를 체크하는 변수
 	int mode;//mode를 선택하도록 한다.
 	int password=1111,inputPassword;//password 초기값과 입력값
 	count = loadData(plist);
 	index = count;
+	index2 = count2;
     password=loadPassword();//비밀번호값 불러오기
 
 	printf("점장이신가요? 손님이신가요? (점장: 1, 손님:2) ");
@@ -75,12 +77,15 @@ if(mode==2){
             menu = selectMenu2();
             if (menu == 0) break;
             if (menu == 1){
-                int check;//주문이 끝났는지 확인하는 변수
-                do{
-                int no = selectProductNo(plist, index);
+               int check;//주문이 끝났는지 확인하는 변수
+                int i=0;
+                do {
+                clist[index2].menuNum[i] = selectProductNo(plist, index);
                 printf("더 주문하시겠습니까?(예:1, 아니요:2)");
                 scanf("%d",&check);
-                }while(check==1);
+                i++;
+                } while(check==1);
+                checkTakeout(&clist[index2++]);
             }
             else if (menu == 2){
                 
