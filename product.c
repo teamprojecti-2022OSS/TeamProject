3018151fa756da80b2e107d975f32293de9d57a8
s21900156 <100466772+s21900156@users.noreply.github.com>
2022-05-13 22:59:25 +0900

    Update product.c
    영수증 출력부분 꾸미기

--- product.c
+++ product.c
@@ -202,8 +202,12 @@ void printReceipt(Custom *c, Product *p,int num){
         if(c[num].menuNum[i]==0) break;
         total+=p[c[num].menuNum[i]-1].price;
     }
-    if(strcmp(c[num].placeEat,"T")==0) printf("총 %d입니다.\n 포장입니다\n",total);
-    else printf("총 %d입니다.\n 매장이용입니다\n",total);
+    printf("\n**************************\n");
+    printf("총 금액    ||  이용방법  *\n");
+    printf("==========================\n");
+    if(strcmp(c[num].placeEat,"T")==0) printf("%-10d ||    포장    *\n",total);
+    else printf("%-10d ||    매장    *\n",total);
+    printf("**************************\n");
 }
 void saveData2(Custom *c, int count){
     int i=0;
