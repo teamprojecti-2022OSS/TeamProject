d40381d8453931c833236cec377d5f835a576242
s21900156 <100466772+s21900156@users.noreply.github.com>
2022-05-13 22:27:22 +0900

    Update product.h
    주차요금 계산하는데 있어 수정이 필요해 수정함.

--- product.h
+++ product.h
@@ -8,7 +8,7 @@ typedef struct {
 	int menuNum[20];//주문하는 메뉴번호
 	char placeEat[5];//포장 or 매장
 	int carNum;//주차번호
-	int cometime;//입장시간
+	int cometime[2];//입장시간(시간과 분을 구분)
 } Custom;
 
 int selectMenu();//선택지 제공(점장모드)
@@ -27,7 +27,8 @@ int loadPassword();//비밀번호 불러오기
 void checkTakeout(Custom *c);//포장인지 주문인지 파악
 void checkCarnum(Custom *c);//차번호와 들어온 시간을 저장
 int printParkingorder(Custom *c,int count);//고객님의 차번호가 있는 순서를 반환해주는 함수
-int printCometime(Custom *c,int CarNum);//고객님의 들어온 시간 반환
+int printComehour(Custom *c,int CarNum);//고객님의 들어온 시간만을 반환
+int printComeminute(Custom *c,int CarNum);//고객님의 들어온 분을 반환
 int calParkingfee(int cometime);//고객님의 들어온 시간을 가지고, 요금을 반환해주는 함수
 void printReceipt(Custom *c, Product *p,int num);//영수증 출력해주는 함수0
 void saveData2(Custom *c, int count); //손님에 대한 FILE 저장
